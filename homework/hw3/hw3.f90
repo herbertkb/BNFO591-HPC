83ef4d6eb22f6dda222c8f263adde84d2fcc0a70
Keith <herbertkb@vcu.edu>
2015-10-20 09:23:53 -0400

    matrix doesn't multiply correctly

--- homework/hw3/hw3.f90
+++ homework/hw3/hw3.f90
@@ -1,7 +1,10 @@
 program hw3
     implicit none
     
-    interface 
+    interface
+        integer function CountLinesInFile(filename) result (lineCount)
+            character(len=*), intent(in) :: filename
+        end function 
         subroutine CollectDataPairs(filename, pairs)
             character(len=*), intent(in) :: filename
             character(len=4), intent(inout) :: pairs(:,:)
@@ -32,48 +35,126 @@ program hw3
             character(len=4), intent(in) :: labels(:)
         end subroutine
         
+        subroutine WriteAdjacencyMatrix(matrix, labels, unit_num)
+            integer, intent(in) :: matrix(:,:)
+            character(len=4), intent(in) :: labels(:)
+            integer :: unit_num
+        end subroutine
+        
+        subroutine PrintLogicalMatrix(matrix, labels)
+            logical, intent(in) :: matrix(:,:)
+            character(len=4), intent(in) :: labels(:)
+        end subroutine
+        
     end interface
     
-    character(len=*),parameter :: file1="testdata.dat"
-    character(len=4) :: labelA, labelB
-    character*4, allocatable :: labels(:), uniqueLabels(:)
-    integer :: iostatus, i, j, n
+    character(len=*),parameter  ::  file1="testdata.dat"
+    character(len=*),parameter  ::  fileout="testoutput_herbertkb.dat"    
+    integer :: iostatus, i, j,k, n
     integer :: countOfEdges, countUniqueLabels
-    integer :: CountLinesInFile, CollectEdgesFromFile
-    character*4, dimension(:, :), allocatable :: edges
+    character*4, allocatable :: edges(:,:)
+    character*4, allocatable :: uniqueLabels(:)
     integer, dimension(:, :), allocatable :: adjMatrix
-    integer :: index_a, index_b
-    character(len=4) :: a, b
+    logical, allocatable :: nonZeroMatrix(:,:)
+    logical :: flagAllTrue
+    integer :: diameter
     
+    !! open output file for writing
+    open(unit=10, file=fileout, status='replace')
     
+    !! First, count #edges described in file
     countOfEdges = CountLinesInFile(file1)
     allocate( edges(countOfEdges, 2))
 
     print '("count of edges: ", I5)', countOfEdges
+    write(10, '("count of edges: ", I5)') countOfEdges
     
+    !! Next, collect the edges into a two column array
     call CollectDataPairs(file1, edges)
     
-    
-    do i=1,countOfEdges
-        print *, edges(i,1), edges(i,2)
-    end do
-    
+    !! And find the unique nodes in the edge list.
     ! worst case: every node is unique so #nodes is twice #edges 
     allocate(uniqueLabels( 2 * countOfEdges))
-    
     call GetUniqueValues(edges, uniqueLabels)
-    
     countUniqueLabels = size(uniqueLabels)
+    
     print '("count of unique nodes:", I5)', countUniqueLabels
+    write(10, '("count of unique nodes:", I5)') countUniqueLabels
     
+    !! Sort the label list so the Adjancency Matrix is readable
     call SelectionSort(uniqueLabels)
-    
     
     !! Build Adjancency Matrix    
     call BuildAdjacencyMatrix(adjMatrix, edges, uniqueLabels)
     
     !! print Adjancency Matrix
     call PrintAdjacencyMatrix(adjMatrix, uniqueLabels)
+    call WriteAdjacencyMatrix(adjMatrix, uniqueLabels, 10)
+    
+    !!Find the diameter of the graph for the Adjancency Matrix
+    !!===============================================================
+    !! do i=1,N-1
+    !!      multiply matrix by itself
+    !!      if all elements take a value > 0
+    !!          d = i
+    !!          break    
+    !! end loop
+    
+    !! Create a matrix of booleans to indicate when each cell is nonzero atleast once
+    allocate(nonZeroMatrix (countUniqueLabels, countUniqueLabels) )
+    do i=1, size(nonZeroMatrix, 1)
+        do j=1, size(nonZeroMatrix, 1)
+            if(adjMatrix(i,j) > 0 ) then
+                nonZeroMatrix(i,j) = .TRUE.
+            else
+                nonZeroMatrix(i,j) = .FALSE.
+            end if
+        end do
+    end do
+!    call PrintLogicalMatrix(nonZeroMatrix, uniqueLabels)
+
+    
+    do i=1, countUniqueLabels-1
+        
+        !! Multply the matrix by itself
+        adjMatrix = matmul(adjMatrix, adjMatrix)
+        
+!        call PrintAdjacencyMatrix(adjMatrix, uniqueLabels)
+        
+        !! set all the corresponding cells in the truth matrix to true
+        do j=1, size(nonZeroMatrix, 1)
+            do k=1, size(nonZeroMatrix, 1)
+                if(adjMatrix(j,k) > 0 ) then
+                    nonZeroMatrix(j,k) = .TRUE.
+                end if
+            end do
+        end do
+        
+!        print *
+!        call PrintLogicalMatrix(nonZeroMatrix, uniqueLabels)
+        
+        !! Check if the entire truth matrix is true
+        flagAllTrue = .TRUE.
+        do j=1, size(nonZeroMatrix, 1)
+            do k=1, size(nonZeroMatrix, 1)
+                if( nonZeroMatrix(j,k) .EQV. .FALSE. ) then
+                    flagAllTrue = .FALSE.
+                end if
+            end do
+        end do
+        
+        !! If it is, save the diameter and stop looping
+        if(flagAllTrue .EQV. .TRUE.) then
+            diameter = i
+            exit
+        end if
+
+    end do
+    
+    print *, diameter
+    write(10, '("diameter of graph:", I5)') diameter
+    
+    close(10)
     
 end program hw3
 
@@ -111,11 +192,11 @@ subroutine CollectDataPairs(filename, pairs)
     n = size(pairs,1) 
 
     
-    open(unit=11, file=filename, status='old')
+    open(unit=12, file=filename, status='old')
         do i=1,n
-            read(11, *, iostat=iostatus) pairs(i,1), pairs(i, 2)   ! a, b
+            read(12, *, iostat=iostatus) pairs(i,1), pairs(i, 2)   ! a, b
        end do
-    close(unit=11)
+    close(unit=12)
     
 end subroutine CollectDataPairs
 
@@ -262,10 +343,38 @@ subroutine PrintAdjacencyMatrix(matrix, labels)
         print '(A4 *(I4))', labels(i), ( matrix(i, j), j=1,n)
     end do
     
+end subroutine
+
+subroutine WriteAdjacencyMatrix(matrix, labels, unit_num)
+    integer, intent(in) :: matrix(:,:)
+    character(len=4), intent(in) :: labels(:)
+    integer, intent(in) :: unit_num 
+    integer :: n
+    
+    n = size(matrix, 1)
+    
+    write(unit_num, '(A7 *(A4))') (labels(i), i=1,n)
+    do i=1,n
+        write(unit_num, '(A4 *(I4))') labels(i), ( matrix(i, j), j=1,n)
+    end do
     
 end subroutine
 
 
+
+subroutine PrintLogicalMatrix(matrix, labels)
+    logical, intent(in) :: matrix(:,:)
+    character(len=4), intent(in) :: labels(:)
+    integer :: n
+    
+    n = size(matrix, 1)
+    
+    print '(A7 *(A4))', "", (labels(i), i=1,n)
+    do i=1,n
+        print '(A4 *(L4))', labels(i), ( matrix(i, j), j=1,n)
+    end do
+    
+end subroutine
 
 
 

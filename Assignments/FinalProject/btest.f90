b7e8a6e1c82da16b25033e3d90a24b68873d2d35
Keith <herbertkb@vcu.edu>
2015-12-07 23:00:49 -0500

    cleaning variable declarations for powerset.f90

--- Assignments/FinalProject/btest.f90
+++
@@ -1,9 +0,0 @@
-program test_btest
-  integer :: i = 32768 + 1024 + 64
-  integer :: pos
-  logical :: bool
-  do pos=0,16
-      bool = btest(i, pos)
-      print *, pos, bool
-  end do
-end program test_btest
--- Assignments/FinalProject/powerset.f90
+++ Assignments/FinalProject/powerset.f90
@@ -1,4 +1,4 @@
-!! orderbyalpha.f90
+!! powreset.f90
 !! Keith Herbert
 !! BNFO591 Final Project, part 2
 !!
@@ -13,8 +13,23 @@
 !! where the first number is the index of the subset,
 !! other numbers to the left of -999 are in a subset,
 !! and those to the right are not
-
-
+!!
+!! The subsets of a set can be modeled as a binary expansion with each
+!! digit representing if a given element of the set is in a particular
+!! subset. Given S = {a, b, c}, we can model subsets as 
+!! 000 = {}
+!! 001 = {a}
+!! 010 = {b}
+!! 011 = {a, b}
+!! 100 = {c}
+!! 101 = {a, c}
+!! 110 = {b, c}
+!! 111 = {a, b, c}
+!!
+!! This program works by taking the index of the current subset and converting
+!! it to its binary string as a logical array. The logical array is parsed
+!! to generate the output line.
+!!
 program powerset
     implicit none
 
@@ -27,46 +42,33 @@ program powerset
 
     character(len=*), parameter :: IN_FILE  = "Alphabetical_ICD.dat" 
     character(len=*), parameter :: OUT_FILE = "all_possible_subsets.dat"
-    integer(kind=1), parameter  :: OUT_UNIT = 100 
-    
-    integer, parameter :: HOW_FAR = 100     !! how many subsets to output
-    integer, allocatable :: code_indices(:)
-    integer :: code_size
-    logical, allocatable :: subset(:)
-    integer :: i,j                          !! index variable
-    integer(kind=8) :: temp 
-    integer(kind=1) :: digit
+    integer(kind=1), parameter  :: OUT_UNIT = 100
+    logical,parameter           :: WRITE_TO_FILE = .TRUE.
+    integer, parameter          :: HOW_FAR = 100
+   
+    integer :: code_size                !! how many codes provided
+    logical, allocatable :: subset(:)   !! indicates a given subset of codes
+    integer :: i,j                      !! index variables
+    integer(kind=8) :: subset_index     !! the current subset 
+    integer(kind=8) :: temp             !! a copy of subset_index destroyed
+                                        !!   during conversion to binary string
+    integer(kind=1) :: digit            !! the binary digit shaved off on each
+                                        !!   pass of the conversion process.
      
     
     print *, "Reading ", IN_FILE, " and getting code indices"
-    allocate( code_indices( CountLinesInFile(IN_FILE) ) )
-    code_size = size(code_indices)
-    do i=1,code_size
-        code_indices(i) = i
-    enddo
-    
+    code_size = CountLinesInFile(IN_FILE)
     print *, code_size, " codes in ", IN_FILE
-    
-    !! The subsets of a set can be modeled as a binary expansion with each
-    !! digit representing if a given element of the set is in a particular
-    !! subset. Given S = {a, b, c}, we can model subsets as 
-    !! 000 = {}
-    !! 001 = {a}
-    !! 010 = {b}
-    !! 011 = {a, b}
-    !! 100 = {c}
-    !! 101 = {a, c}
-    !! 110 = {b, c}
-    !! 111 = {a, b, c}
+   
     
     print *, "Calculating ICD10 code subsets and writing to ", OUT_FILE
     open(unit=OUT_UNIT, file=OUT_FILE)
     allocate(subset(code_size))
     
-    do i=1,HOW_FAR
+    do subset_index=1,HOW_FAR
         !! First we need need to convert the current index to a binary string
         !! stored in a logical array.
-        temp = i
+        temp = subset_index
         do j=1,code_size
             
             if (temp < 0) then 
@@ -85,7 +87,7 @@ program powerset
         end do
 
         !! Then print the subset represented by the array
-        write(OUT_UNIT, '(I20)', ADVANCE="NO") i 
+        write(OUT_UNIT, '(I15)', ADVANCE="NO") subset_index 
         do j=1, code_size
             if(subset(j)) then
                write(OUT_UNIT, '(I2)', ADVANCE="NO") j

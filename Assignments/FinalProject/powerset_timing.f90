ebc66deefd35b11148933b64b887f9acf5e8590a
Keith <herbertkb@vcu.edu>
2015-12-08 01:08:19 -0500

    crushed added timing with powerset_timing.f90

--- Assignments/FinalProject/powerset_timing.f90
+++ Assignments/FinalProject/powerset_timing.f90
@@ -42,94 +42,56 @@ program powerset
             integer(kind=8)        :: indx
             logical, intent(inout) :: subset(:)
         end subroutine
-        
-        subroutine WriteSubset(u, ind, subset)
-            integer(kind=1), intent(in) :: u   !! logical unit for output
-            integer(kind=8), intent(in) :: ind !! index of subset to start line
-            logical, intent(inout) :: subset(:)!! subset to be printed
-        end subroutine
-
+      
     end interface
     
 
-    character(len=*), parameter :: IN_FILE  = "Alphabetical_ICD.dat" 
-    character(len=*), parameter :: OUT_FILE = "all_possible_subsets.dat"
-    integer(kind=1), parameter  :: OUT_UNIT = 100
+    character(len=*), parameter :: IN_FILE       = "Alphabetical_ICD.dat" 
+    character(len=*), parameter :: OUT_FILE      = "no_opt_timing.dat"
+    integer(kind=1), parameter  :: OUT_UNIT      = 100
     logical,parameter           :: WRITE_TO_FILE = .TRUE.
-    integer, parameter          :: HOW_FAR = 100
+    integer, parameter          :: N             = 10
    
     integer :: code_size                !! how many codes provided
     logical, allocatable :: subset(:)   !! indicates a given subset of codes
-    integer :: i,j                      !! index variables
+    integer :: i                        !! index variables
     integer(kind=8) :: subset_index     !! the current subset 
     integer(kind=8) :: temp             !! a copy of subset_index destroyed
                                         !!   during conversion to binary string
     integer(kind=1) :: digit            !! the binary digit shaved off on each
                                         !!   pass of the conversion process.
-     
     
-    print *, "Reading ", IN_FILE, " and getting code indices"
-    code_size = CountLinesInFile(IN_FILE)
-    print *, code_size, " codes in ", IN_FILE
-   
+    integer(kind=8) :: time_start, time_end, rate !! timing vales
+    integer(kind=8) :: times(N)                  !! timing array
+       
+    allocate(subset(CountLinesInFile(IN_FILE)))
     
-    print *, "Calculating ICD10 code subsets and writing to ", OUT_FILE
     open(unit=OUT_UNIT, file=OUT_FILE)
-    allocate(subset(code_size))
     
-    do subset_index=1,HOW_FAR
-        !! First we need need to convert the current index to a binary string
-        !! stored in a logical array.
-        call ConvertIndexToSubsetArray(subset_index, subset)
-
-        !! Then print the subset represented by the array
-        if(WRITE_TO_FILE) then
-            call WriteSubset(OUT_UNIT, subset_index, subset)
-        end if
-    enddo
+    do i = 0, N
+        call system_clock(time_start, rate)
+        
+        do subset_index=1,10**i
+            call ConvertIndexToSubsetArray(subset_index, subset)
+        enddo
+        
+        call system_clock(time_end)
+        
+        times(i) = time_end - time_start
+        
+        print *, i, times(i)
+        write(OUT_UNIT, *) i, times(i)
+    end do
+    
     close(OUT_UNIT)
-    
-    print *, "Finished."
 
 end program
 
-subroutine WriteSubset(u, ind, subset)
-    implicit none
-    integer(kind=1), intent(in) :: u     !! logical unit for output
-    integer(kind=8), intent(in) :: ind   !! index of subset to start line
-    logical, intent(inout) :: subset(:)  !! subset to be printed
-    integer :: j                         !! index variable 
-
-    !! print the index at start of line
-    write(u, '(I15)', ADVANCE="NO") ind
-    
-    !! print the element IDs in the subset 
-    do j=1, size(subset)
-        if(subset(j)) then
-           write(u, '(I2)', ADVANCE="NO") j
-        endif
-    end do
-    
-    !! this separates the ins from the outs
-    write(u, '(I5)', ADVANCE = "NO") -999
-    
-    !! print the element IDs NOT in the subset
-    do j=1, size(subset)
-        if(.NOT. subset(j)) then
-           write(u, '(I4)', ADVANCE="NO") j
-        endif
-    end do
-    
-    !! new line
-    write(u, '(A)') ""
-    
-end subroutine
-
 subroutine ConvertIndexToSubsetArray(indx, subset)
     implicit none
-    integer(kind=8),intent(in)  :: indx        
-    integer(kind=8)             :: temp
-    logical, intent(inout)      :: subset(:)
+    integer(kind=8),intent(in) :: indx        
+    integer(kind=8)            :: temp
+    logical, intent(inout)     :: subset(:)
     integer :: i
     integer(kind=8) :: digit
     
